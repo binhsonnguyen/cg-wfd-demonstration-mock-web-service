b6e9e0f10174f76c16da8864e6f0c9768ca68963
edward <son@binhsonnguyen.com>
2019-08-08 11:52:59 +0700

    Start server with defined port that config in env.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const server = express();
 const cors = require('cors');
+const PORT = process.env.PORT || 5000;
 server.use(cors());
 server.use(express.json());
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 });
 
 let autoIncrementId = 0;
